75d833e5fa33ce23741da29c9610ee62f4d823e9
William Assis <williamf1f2@gmail.com>
2020-01-15 00:24:39 -0300

    Criação de controller para pesquisa de devs

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,33 +1,46 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 
+// o controller geralmente tem 5 funções:
+// index, show, store, update, destroy
+
 module.exports = {
+    async index(req, res) {
+        const devs = await Dev.find();
+
+        return res.json(devs);
+    },
+
     async store (req, res) {
         const { github_username, techs, latitude, longitude } = req.body; // , latitude, longitude
         // console.log(github_username);
+
+        let dev = await Dev.findOne({ github_username });
+
+        if (!dev) {
+            const response = await axios.get(`https://api.github.com/users/${github_username}`);
     
-        const response = await axios.get(`https://api.github.com/users/${github_username}`);
-    
-        const { name = login, avatar_url, bio } = response.data;
-    
-        console.log(name, avatar_url, bio);
-    
-        const techsArray = techs.split(',').map(tech => tech.trim()); //tring remove espaços antes e no final da string
-    
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude],
-        };
-    
-        // short sintaxe do objeto sem precisar repetir os nomes das variaveis
-        const dev = await Dev.create({
-            name,
-            github_username,
-            bio,
-            avatar_url,
-            techs: techsArray,
-            location
-        });
+            const { name = login, avatar_url, bio } = response.data;
+        
+            console.log(name, avatar_url, bio);
+        
+            const techsArray = techs.split(',').map(tech => tech.trim()); //tring remove espaços antes e no final da string
+        
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            };
+        
+            // short sintaxe do objeto sem precisar repetir os nomes das variaveis
+            dev = await Dev.create({
+                name,
+                github_username,
+                bio,
+                avatar_url,
+                techs: techsArray,
+                location
+            }); 
+        }
     
         return res.json(dev);
     }
---
+++ src/controllers/SearchController.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const { DevController } = require('../models/Dev');
+
+module.export = {
+    async index(req, res) {
+        // buscar todos os devs em um raio de 10km
+        // filtrar por tecnologias usadas
+        
+    }
+}
